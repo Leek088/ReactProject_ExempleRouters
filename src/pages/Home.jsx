e00c52f98d41fdc11de676cdfed5a4bbb4b98f91
Leek088 <leonardoleeko88@gmail.com>
2024-06-24 21:41:50 -0300

    Fim do estudo da noite.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
-﻿function Home() {
+﻿import './Home.css'
+import { useApiRequest } from '../hooks/useApiRequest'
+function Home() {
+    const urlApiProducts = "http://localhost:3000/products"
+    const { data, loading, error } = useApiRequest(urlApiProducts);
     return (
         <>
-            <p>Você está na página Home!</p>
+            <h1>Produtos cadastrados:</h1>
+            {error && <div>{error}</div>}
+            {loading
+                ? (<div>Carregando...</div>)
+                : (
+                    <ul className="products">
+                        {
+                            data && data.map((product) => (
+                                <li key={product.id}>
+                                    <h2>{product.name}</h2>                                   
+                                    <p>Preço: R$ {product.price}</p>
+                                    <p>Disponível:&nbsp;
+                                        {product.available ? "Sim" : "Não"}
+                                    </p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                )
+            }
         </>
     );
 }
